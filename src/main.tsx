9ccc5e5e2e5486ff86393d84aa7070f97084ef47
yuzhang9804 <yuzhang9804@gmail.com>
2024-08-03 17:19:11 +0800

    chore: init

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import { App } from './components/App'
+import '@radix-ui/themes/styles.css'
+import './index.css'
+
+const dom = document.getElementById('root')
+createRoot(dom!).render(<App />)
